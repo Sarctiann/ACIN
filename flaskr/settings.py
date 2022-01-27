514772c1066313da61b878a9230de90e5614f71a
Sarctiann <sebas.sarc@gmail.com>
2022-01-27 11:48:47 -0300

    first try

--- flaskr/settings.py
+++ flaskr/settings.py
@@ -6,15 +6,9 @@ JWT_SECRET_KEY = SECRET_KEY
 
 MONGODB_SETTINGS = {
 
-    #BASIC:
-    'host': os.environ.get('MONGODB_HOST'),
-    'port': int(os.environ.get('MONGODB_PORT')),
-    'db': os.environ.get('MONGODB_DB'),
-
-    # AUTHENTICATION:
-    'username': os.environ.get('MONGODB_USER'),
-    'password': os.environ.get('MONGODB_PASS'),
-    'authentication_source': os.environ.get('MONGODB_SOURCE'),
-    'authentication_mechanism': os.environ.get('MONGODB_AUTH_MECH')
+    'host': os.environ.get('MONGODB_HOST').format(
+        user=os.environ.get('MONGODB_USER'),
+        pasw=os.environ.get('MONGODB_PASS')
+    )
 }
 JWT_EXPIRES = dt.timedelta(hours=24)
