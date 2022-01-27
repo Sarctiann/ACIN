67515a7947954ac41504feeb972f4f9fbe14e391
Sarctiann <sebas.sarc@gmail.com>
2022-01-27 17:49:18 -0300

    new try

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
+import os
 from dotenv import load_dotenv
+
+BASE_DIR = os.path.abspath(os.path.dirname(__name__))
+project_folder = os.path.expanduser(BASE_DIR)
+load_dotenv(os.path.join(project_folder, '.env'))
+
 load_dotenv('.env')
 
 from flaskr import create_app
