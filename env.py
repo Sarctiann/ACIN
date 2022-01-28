87042aa6d7868544e67a80b111d929959b79c10c
Sarctiann <sebas.sarc@gmail.com>
2022-01-27 21:25:46 -0300

    let's see

--- env.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY="sebastián-atlántico-rodríguez-capurro_audiocenter_2021"
-MONGODB_HOST="mongodb+srv://{user}:{pasw}@asincluster.nwvpm.mongodb.net/acin_db?retryWrites=true&w=majority"
-MONGODB_USER="Sarctiann"
-MONGODB_PASS="acin db sarc"
--- flaskr/settings.py
+++ flaskr/settings.py
@@ -1,16 +1,15 @@
-# import os
+import os
 import datetime as dt
 
-import env
 
-SECRET_KEY = env.SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET_KEY = SECRET_KEY
 
 MONGODB_SETTINGS = {
 
-    'host': env.MONGODB_HOST.format(
-        user=env.MONGODB_USER,
-        pasw=env.MONGODB_PASS
+    'host': os.environ.get('MONGODB_HOST').format(
+        user=os.environ.get('MONGODB_USER'),
+        pasw=os.environ.get('MONGODB_PASS')
     )
 }
 JWT_EXPIRES = dt.timedelta(hours=24)
